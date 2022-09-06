6854f9ea78759fd41950586549ee313acdea0e82
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-06 01:18:40 +0000

    Adding router to express server file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import router from './routes/index';
 
 const app = express();
+router(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
